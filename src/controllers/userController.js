3991f24d66d04c31df5c7b0439b6559edd7b6a6d
nirmalasharma123 <jassusharma159@gmail.com>
2022-12-17 13:51:19 +0530

    assignmrnt mongodb

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,14 @@
-const UserModel= require("../models/userModel")
+const allBooks=require("../models/userModel.js")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
+    const newBooks=async function(req,res){
+        let data= req.body
+        let allbooks = await allBooks.create(data)
+        res.send(allbooks)
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+      }
+    const allBooksIn = async function(req,res){
+        let allbk=await allBooks.find();
+        res.send(allbk)
+    }
+      module.exports.allNewbooks=newBooks;
+      module.exports.allnewbk=allBooksIn;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+ const express = require('express');
+ const router = express.Router();
+const allBooks = require("../models/userModel.js");
+const newBoks=require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
-
-module.exports = router;
+router.post("/newbooks", newBoks.allNewbooks);
+router.get("/allBooks", newBoks.allnewbk);
+module.exports=router;
+ 
