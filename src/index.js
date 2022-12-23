5aa3fa35d41d96dc55ce4f56081bbfac36aa6bbe
nirmalasharma123 <jassusharma159@gmail.com>
2022-12-23 15:23:48 +0530

    mdlw1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const mdlWare=require("./middlewares/commonMiddlewares.js")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,13 +15,7 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
-
+app.use (mdlWare.mid1);
 app.use('/', route);
 
 
